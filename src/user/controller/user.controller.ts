c8ebc11a51b4fffeb6a4cb291b9aecb6c445da14
saeed <s8990@yahoo.com>
2022-02-03 01:27:17 +0330

    Add User Module-Service-Controller-Models

---
+++ src/user/controller/user.controller.ts
@@ -0,0 +1,43 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { User } from '../models/user.interface';
+import { UserService } from '../service/user.service';
+import { Observable } from 'rxjs';
+import { DeleteDateColumn } from 'typeorm';
+
+@Controller('users')
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Post()
+  create(@Body() user: User): Observable<User> {
+    return this.userService.create(user);
+  }
+
+  @Get(':id')
+  findOne(@Param() params): Observable<User> {
+    return this.userService.findOne(params.id);
+  }
+
+  @Get()
+  findAll(): Observable<User[]> {
+    return this.userService.findAll();
+  }
+
+  @Delete(':id')
+  deleteOne(@Param('id') id: string): Observable<User> {
+    return this.userService.deleteOne(Number(id));
+  }
+
+  @Put(':id')
+  updateOne(@Param('id') id: string, @Body() user: User): Observable<any> {
+    return this.userService.updateOne(Number(id), user);
+  }
+}
